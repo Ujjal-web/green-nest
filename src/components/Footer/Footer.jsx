718a09671bde9eb284069d4fd327efe75dd0f6cc
ujjal-web <ujjaldas827@gmail.com>
2025-10-23 01:46:04 +0600

    header and footer added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { FaFacebook, FaInstagram, FaPinterest } from 'react-icons/fa';
+import { NavLink } from 'react-router';
 
 const Footer = () => {
     return (
         <div>
-            <h2>This is footer</h2>
+            <footer className="footer footer-horizontal footer-center bg-base-200 text-base-content rounded p-10">
+                <nav className="grid grid-flow-col gap-4">
+                    <a className="link link-hover">About</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Privacy Policy</a>
+                </nav>
+                <nav>
+                    <div className="grid grid-flow-col gap-4 text-4xl">
+                        <NavLink to="https://www.instagram.com/"><FaInstagram /></NavLink>
+                        <NavLink to="https://www.facebook.com/"><FaFacebook /></NavLink>
+                        <NavLink to="https://www.pinterest.com/"><FaPinterest /></NavLink>
+
+                    </div>
+                </nav>
+                <aside>
+                    <p>© {new Date().getFullYear()} <span className='text-green-700 georama-font text-lg font-bold'>GreenNest</span>. All rights reserved</p>
+                </aside>
+            </footer>
         </div>
     );
 };
